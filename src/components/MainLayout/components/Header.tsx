e1eeb88f18503e6a6976c1a31715f33f2356016f
Shagon1k (Aliaksei Hurynovich) <shagon1k@gmail.com>
2023-02-14 16:05:18 +0200

    [Task-1] Add thematic (Coffee) to store app

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            Coffee Shop
           </Link>
         </Typography>
 
