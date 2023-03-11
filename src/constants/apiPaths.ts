bc51768346bd6aaedcd71bc0a17e689f124dc92e
Shagon1k (Aliaksei Hurynovich) <shagon1k@gmail.com>
2023-03-11 18:10:03 +0200

    [Task-5] Integrate FE app with initial Import Service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://1yls2gee5l.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://ejv63o8s93.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
