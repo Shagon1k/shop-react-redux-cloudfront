7e61c453b3354a1b2fc37fd933c569e688c6e443
Shagon1k (Aliaksei Hurynovich) <shagon1k@gmail.com>
2023-03-26 13:27:59 +0300

    [Task-7] Update Shop App import products with authentication

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import axios from "axios";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import { getAuthorizationToken } from "~/utils/utils";
 
 type CSVFileImportProps = {
   url: string;
@@ -26,10 +27,19 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
   const uploadFile = async () => {
     console.log("uploadFile to", url);
 
+    const authToken = getAuthorizationToken();
+    const headers = {
+      ...(authToken && {
+        Authorization: `Basic ${authToken}`,
+      }),
+    };
+
+
     // Get the presigned URL
     const response = await axios({
       method: "GET",
       url,
+      headers,
       params: {
         name: encodeURIComponent(file?.name || ""),
       },
